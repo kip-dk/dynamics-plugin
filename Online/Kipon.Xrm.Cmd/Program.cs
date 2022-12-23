614af43220bc2eefea845180e0818d75fb8791ab
Kjeld Poulsen <kip@boruto.com>
2022-12-23 08:06:59 +0100

    prepared generate entities for new online version

---
+++ Online/Kipon.Xrm.Cmd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kipon.Xrm.Cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Online/Kipon.Xrm.Cmd/Tools/Deploy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kipon.Xrm.Cmd.Tools
+{
+    [Export("deploy", typeof(ICmd))]
+    public class Deploy : ICmd
+    {
+        private Kipon.Xrm.Tools.Deploy.Plugins pluginDeployer;
+
+        [ImportingConstructor]
+        public Deploy(Kipon.Xrm.Tools.Deploy.Plugins pluginDeployer)
+        {
+            this.pluginDeployer = pluginDeployer;
+        }
+
+        public Task ExecuteAsync(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Please provide path to assembly as first parameter.");
+                Console.ReadLine();
+                return Task.CompletedTask;
+            }
+
+            Console.WriteLine("Starting deployment tool..." );
+
+            this.pluginDeployer.Run(args[0]);
+            return Task.CompletedTask;
+        }
+    }
+}
