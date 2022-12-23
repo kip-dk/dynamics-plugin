0014ac9d532767bce4e2547c867ace8e6262d1bf
Kjeld Poulsen <kip@boruto.com>
2022-12-23 08:21:12 +0100

    moved plugin lib again

--- Online/Kipon.Online.Plugin/Entities/generate.cmd
+++
@@ -1,3 +0,0 @@
-﻿@echo off
-copy ..\..\Kipon.Xrm.Tools\bin\debug\Kipon.Xrm.Tools.* ..\..\Kipon.Xrm.Tools\bin\coretools
-..\..\Kipon.Xrm.Tools\bin\coretools\CrmSvcUtil.exe "/connectionstring:AuthType=AD;Url=http://kipon-dev/kip;" /out:Context.design.cs /ServiceContextName:SolidContextService /namespace:Kipon.Online.Plugin.Entities /codewriterfilter:Kipon.Xrm.Tools.CodeWriter.CodeWriterFilter,Kipon.Xrm.Tools /codecustomization:Kipon.Xrm.Tools.CodeWriter.PluginCustomizeCodeDomService,Kipon.Xrm.Tools /generateActions /debug
