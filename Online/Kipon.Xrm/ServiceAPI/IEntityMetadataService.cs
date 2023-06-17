549a61de00aa0dab329eb5a225bbf07b655ae7fe
Kjeld Poulsen <kip@boruto.com>
2023-06-17 16:23:28 +0200

    added ientitymetaservice to upcomming 2 0 release ... not pushed to git yet

---
+++ Online/Kipon.Xrm/ServiceAPI/IEntityMetadataService.cs
@@ -0,0 +1,12 @@
+﻿namespace Kipon.Xrm.ServiceAPI
+{
+    public interface IEntityMetadataService
+    {
+        Microsoft.Xrm.Sdk.Metadata.EntityMetadata ForEntity(string logicalName);
+        string[] CanCreateAttributeName(string logicalName);
+        string[] CanUpdateAttributeName(string logicalName);
+        string PrimaryKey(string logicalName);
+        string PrimaryName(string logicalName);
+        Microsoft.Xrm.Sdk.Entity CloneForCreate(Microsoft.Xrm.Sdk.Entity source, params string[] ommit);
+    }
+}
